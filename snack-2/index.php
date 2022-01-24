7b93d267c9c87ed896c24d82fab3f95260632aef
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-24 16:56:28 +0100

    Snack.2 - Completato verifiche dati

--- snack-2/index.php
+++ snack-2/index.php
@@ -7,11 +7,51 @@ una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riusc
 <?php 
     $user_name = $_GET["name"];
     $user_mail = $_GET["mail"];
-    $user_age = $_GET["age"];
+    $user_age = intval($_GET["age"]);
 
-    var_dump($user_name);
-    var_dump($user_mail);
-    var_dump($user_age);
+    // Verify User Name
+    $verify_user_name = false;
+    if(strlen($user_name) >= 3) {
+        $verify_user_name = true;
+    }
+
+    // Verify User Mail
+    $verify_user_mail = false;
+    if(strpos($user_mail, '@') !== false && strpos($user_mail, '.') !== false) {
+        $verify_user_mail = true;
+    }
+
+    // Verify User Age
+    $verify_user_age = false;
+    if($user_age === 0) {
+        $verify_user_age = false;
+    } else if(is_int($user_age)) {
+        $verify_user_age = true;
+    }
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-2</title>
+</head>
+<body>
+    <main>
+
+        <h1>Accedi</h1>
+        <?php if( $verify_user_name && $verify_user_mail && $verify_user_age ) {?>
+            <h2>ACCESSO RIUSCITO</h2>
+        <?php } else {?>
+            <h2>ACCESSO NEGATO</h2>
+        <?php } ?>
+
+        <h4>Instruction: ?name=YourName&mail=YourMail&age=YourAge</h4>
+
+        
+    </main>
+</body>
+</html>
