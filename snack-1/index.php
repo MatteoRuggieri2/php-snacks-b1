f2a36d33f7741cc730af2eb760dbe51900e76031
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-24 15:57:28 +0100

    Stampato le partite di basket

--- snack-1/index.php
+++ snack-1/index.php
@@ -26,3 +26,41 @@ Olimpia Milano - Cantù | 55-60 -->
         ]
         ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-1</title>
+</head>
+<body>
+    <main>
+
+        <h1>Risultati partite di basket</h1>
+        <?php for ( $i = 0; $i < count($basketMatches); $i++ ) {?>
+            <div>
+
+                <!-- Matches -->
+                <h3>
+
+                    <!-- Basket Teams -->
+                    <span>
+                        <?php echo $basketMatches[$i]['homeTeam']; ?> - <?php echo $basketMatches[$i]['visitingTeam']; ?>
+                    </span>
+                    
+                    <span> | </span>
+
+                    <!-- Score -->
+                    <span>
+                        <?php echo $basketMatches[$i]['visitingTeamScore']; ?>-<?php echo $basketMatches[$i]['visitingTeamScore']; ?>
+                    </span>
+                </h3>
+
+            </div>
+        <?php } ?>
+        
+    </main>
+</body>
+</html>
