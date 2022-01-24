b7a35d33988e69a1e2fdb679300bc50e39a9f43a
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-24 16:07:32 +0100

    Snack.2 - Salvato input GET in variabili

--- snack-2/index.php
+++ snack-2/index.php
@@ -3,3 +3,15 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
 nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e 
 una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti 
 “Accesso negato”. -->
+
+<?php 
+    $user_name = $_GET["name"];
+    $user_mail = $_GET["mail"];
+    $user_age = $_GET["age"];
+
+    var_dump($user_name);
+    var_dump($user_mail);
+    var_dump($user_age);
+
+?>
+
