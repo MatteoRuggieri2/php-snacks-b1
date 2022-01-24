0dcb5884210a6a104171e9e2d65abbf690057ab1
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-24 15:40:08 +0100

    Snack.1 - Creato array squadre basket

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,1 +1,28 @@
-test snack 1
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa 
+e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    $basketMatches = [
+        [
+            'homeTeam' => 'Trieste',
+            'visitingTeam' => 'Venezia',
+            'homeTeamScore' => '113',
+            'visitingTeamScore' => '117'
+        ],
+        [
+            'homeTeam' => 'Pesaro',
+            'visitingTeam' => 'rimini',
+            'homeTeamScore' => '105',
+            'visitingTeamScore' => '114'
+        ],
+        [
+            'homeTeam' => 'Lecce',
+            'visitingTeam' => 'Pescara',
+            'homeTeamScore' => '91',
+            'visitingTeamScore' => '113'
+        ]
+        ];
+?>
