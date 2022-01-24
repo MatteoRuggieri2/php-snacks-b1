649b03212a782d082363a91e57056663b3c29898
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-24 18:00:42 +0100

    Snack.6 - Consegna

---
+++ snack-6/index.php
@@ -0,0 +1,6 @@
+<!-- ## Snack 6
+Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, 
+Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media 
+dei voti di ogni alunno. -->
+
+test
